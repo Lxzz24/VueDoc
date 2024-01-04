ed52a438c4b4600cbf2f8cf0e167d616778db27b
Lxzz24 <lxzz108@gmail.com>
2024-01-04 16:37:30 +0800

    base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-    base: "/vuepress2/",
+    base: "/VueDoc/",
 
     lang: "zh-CN",
     title: "文档演示",
